fe4d259dc1c218a2483ac537e0a490860645892d
SameenMehmood <sameenmehmood5@gmail.com>
2025-12-27 13:41:51 +0500

    Add files via upload

---
+++ function_demo.py
@@ -0,0 +1,13 @@
+# Function to greet user
+def greet():
+    print("Hello, welcome to Python functions!")
+
+# Function to add two numbers
+def add_numbers():
+    a = 5
+    b = 3
+    print("Sum is:", a + b)
+
+# Function calls
+greet()
+add_numbers()
