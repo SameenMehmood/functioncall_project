c26a560b416e3b107d3de02a0e320dc8def32cea
Sameen Mehmood <sameenmehmood5@gmail.com>
2025-12-28 20:56:41 +0500

    adds another func of subtraction

---
+++ function_demo.py
@@ -0,0 +1,17 @@
+# Function to greet user
+def greet():
+    print("Hello, welcome to Python functions!")
+
+# Function to add two numbers
+def add_numbers():
+    a = 5
+    b = 3
+    print("Sum is:", a + b)
+def subt_numbers():
+    a=4
+    b=1
+    print("remaining is:", a-b)
+# Function calls
+greet()
+add_numbers()
+subt_numbers()
